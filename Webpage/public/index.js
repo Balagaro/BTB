a2ba9c3b76ba0dada751e7988b90e3a9396c4e6e
Balagaro <kiraly123balazs@gmail.com>
2024-10-02 12:21:31 +0200

    Website updates - legal

---
+++ Webpage/public/index.js
@@ -0,0 +1,109 @@
+let text=document.querySelector('.innertext');
+function main(){
+    text.innerHTML=`
+    <div class="liltext">BTB is an Android based mobile game. This survivor game implements AI,
+                    which were meant to make the gameplay much more interesting and fun.
+                </div>
+
+                <div class="liltext">This game was developed and maintained by Hungarian students, for a workshop typed competition,
+                    called Dusza Árpád Programműhely.</div>
+
+                <div class="liltext">We managed to learn a lot of new technologies, new IDEs and new programming
+                    languages - and of course - we had a lot of fun during the project.
+                </div>
+                <div class="liltext">We suggest everyone should give it a try, and please feed back your thought and ideas
+                    about this game.</div>
+                <div class="liltext">Thank you, and have fun:)</div>
+    `;
+}
+
+function legal(){
+    text.innerHTML=`
+    <div class="liltitle">
+                    Welcome to Our World!
+                </div>
+                <div class="liltext">
+                    Welcome to our game! This is a fun adventure where you can explore new worlds. These rules are here to make sure everyone has a great time playing.
+
+                </div>
+                <div class="liltitle">
+                    Age Requirement
+                </div>
+                <div class="liltext">
+
+                    This game is recommended for ages 6 and up. If you are under 6, please ask a parent or guardian before playing.
+                </div>
+
+                <div class="liltitle">
+                    Privacy
+                </div>
+                <div class="liltext">
+                    We don't collect any personal information about you while you play. We only track in-game actions to make the game better.
+
+                </div>
+
+                <div class="liltitle">
+                    How to Play
+                </div>
+                <div class="liltext">
+                    Be kind to other players.
+                    Don't use cheats or hacks.
+                    This game is for personal use only. Don't share your account with others.
+                </div>
+
+                <div class="liltitle">
+                    Parent Involvement
+                </div>
+                <div class="liltext">
+                    A parent or guardian should always be around when you play.
+                </div>
+
+
+                <div class="liltitle">
+                    Changes
+                </div>
+                <div class="liltext">
+                    We might update the game and these rules from time to time.
+                </div>
+
+
+                <div class="liltitle">
+                    Contact
+                </div>
+                <div class="liltext">
+                    If you have any questions, ask a parent or guardian.
+                </div>
+                <div class="liltitle">
+                    Legal
+                </div>
+                <div class="liltext">
+                    This game is governed by the laws of the USA.
+                </div>
+
+                <div class="liltitle">
+                    Additional Tips:
+                </div>
+                <div class="liltext">
+                    Keep it simple and fun: Use easy-to-understand language and avoid legal jargon.
+                    <br>Be positive: Use positive language and focus on the fun aspects of the game.
+                    <br>Be clear: Make sure the rules are clear and easy to follow.
+                    <br>Involve parents: Emphasize the importance of parental involvement.
+                    <br>Consider a FAQ section: Answer common questions that kids might have.
+                    <br>Remember:
+
+                    <br>Legal review: It's always a good idea to have a lawyer review your Terms of Service to ensure it complies with all applicable laws and regulations.
+                    <br>Child-friendly language: Use words and phrases that are easy for children to understand.
+                    <br>Visuals: Consider adding illustrations or graphics to make the Terms of Service more engaging for children.
+
+                </div>
+
+                <div class="liltitle">
+                    Feedback
+                </div>
+                <div class="liltext">
+                    If you have any problem or observation, contact us at ...
+                </div>
+    `
+
+
+}
