dded0edb44e9ed64b232bb4b0d9adada7a93a76d
Capeti0420 <szombathelyipeti@gmail.com>
2024-10-03 21:59:06 +0200

    render stb.

---
+++ Project/app/src/main/java/com/example/btb/Render.kt
@@ -0,0 +1,11 @@
+package com.example.btb
+
+class Render {
+    //SurfaceFlinger
+    // BufferQueue
+    //Android Frame Pacing library
+    //Choreographer
+    //Thread management
+    //GLSurfaceView
+    //eglSwapBuffers()
+}
