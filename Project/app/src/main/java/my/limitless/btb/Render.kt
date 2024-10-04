f99ca2e5ae6737a26267e80023198543c83ca751
Balagaro <kiraly123balazs@gmail.com>
2024-10-04 09:29:23 +0200

    Renderkt frissites

--- Project/app/src/main/java/my/limitless/btb/Render.kt
+++ Project/app/src/main/java/my/limitless/btb/Render.kt
@@ -1,12 +1,8 @@
 package com.example.btb
 
 class Render {
-    //SurfaceFlinger
     // BufferQueue
-    //Android Frame Pacing library
     //Choreographer
     //Thread management
-    //GLSurfaceView
     //eglSwapBuffers()
-    //cigany
 }
